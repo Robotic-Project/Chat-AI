98547d71fc1e3964c26ecb25b2aeb7784ce2e618
김보석 <boseoggim08@gmail.com>
2021-12-14 14:02:55 +0900

    bmo 추적 점 생성

--- FaceDetectionModule.py
+++ FaceDetectionModule.py
@@ -25,28 +25,16 @@ class FaceDetector():
                     img = self.fancyDraw(img, bbox)
 
                     #cv2.rectangle(img, bbox, (0, 255, 0), 2)  # 경계 상자만 출력
-                    cv2.putText(img, f'FPS: {int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)  # 신뢰도 출력
+                    cv2.putText(img, f'acc: {int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)  # 신뢰도 출력
 
         return img, bboxs  # 이미지 반환
 
     def fancyDraw(self, img, bbox, l = 30, t=5, rt = 1):  # 모서리에 강조주는 경계 박스
         x, y, w, h = bbox
-        x1, y1 = x+w, y+h
+        x1, y1 = int(w/2), int(h/4)
 
         cv2.rectangle(img, bbox, (0, 255, 0), rt)  # 경계 상자만 출력
-        # Top Left
-        cv2.line(img, (x, y), (x+l, y), (0, 0, 255), t)
-        cv2.line(img, (x, y), (x, y+l), (0, 0, 255), t)
-        # Top Right
-        cv2.line(img, (x1, y), (x1 - l, y), (0, 0, 255), t)
-        cv2.line(img, (x1, y), (x1, y + l), (0, 0, 255), t)
-        # Bottom Left
-        cv2.line(img, (x, y1), (x + l, y1), (0, 0, 255), t)
-        cv2.line(img, (x, y1), (x, y1 - l), (0, 0, 255), t)
-        # Bottom Right
-        cv2.line(img, (x1, y1), (x1 - l, y1), (0, 0, 255), t)
-        cv2.line(img, (x1, y1), (x1, y1 - l), (0, 0, 255), t)
-
+        cv2.line(img, (x+x1, y+y1), (x+x1+1, y+y1+1), (0, 0, 255), 5)   # 비모의 추적점
         return img
 
 
